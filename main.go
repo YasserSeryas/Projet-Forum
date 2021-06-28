14e293f2ffb2aa56d2a209c17b8e4990f6173709
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2021-06-28 09:56:19 +0200

    Update main.go

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	http.Handle("/insert/", http.NotFoundHandler())
 	http.HandleFunc("/AddComment", h.AddComment)
 	http.Handle("/AddComment/", http.NotFoundHandler())
+	http.HandleFunc("/GetPost", h.GetPost)
 	http.Handle("/GetPost/", http.NotFoundHandler())
 
 	//Load static folder # Front end
